67444cc2c90a0abfec56968e907107575939d7ba
mfernandezfreire <mfernandezfreire@outlook.com>
2020-02-17 17:46:45 +0100

    Terminado LogIn

--- LookingForMaecenas/app.js
+++ LookingForMaecenas/app.js
@@ -10,11 +10,11 @@ const logger       = require('morgan');
 const path         = require('path');
 
 //Passport
-const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
-
+const session = require("express-session");
+const MongoStore = require('connect-mongo')(session);
+const flash = require("connect-flash");
 
 mongoose
   .connect('mongodb://localhost/lookingformecenas', {useNewUrlParser: true})
@@ -46,12 +46,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Express View engine setup
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -59,9 +59,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
+
+// Enable authentication using session + passport
+app.use(session({
+  secret: 'irongenerator',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+app.use(flash());
+require('./passport')(app);
+
 
 
 const index = require('./routes/index');
--- LookingForMaecenas/routes/auths.js
+++ LookingForMaecenas/routes/auths.js
@@ -12,6 +12,12 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
+// ensureLogin
+const ensureLogin = require("connect-ensure-login");
+
+
+
+
 
 //SignUp
 
@@ -20,18 +26,26 @@ router.get("/signup", (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
+  const name = req.body.nombre;
+  const lastName = req.body.apellidos;
+  const email = req.body.email;
+  const phone = req.body.telefono;
   const password = req.body.password;
+  const location = req.body.location;
+  const type = req.body.type;
+  const personalDescription = req.body.personalDescription;
+  const economicContribution = req.body.economicContribution;
+  const project_id = req.body.project_id;
 
-  if (username === "" || password === "") {
+  if (email === "" || password === "") {
     res.render("auths/signup", { message: "Indicate username and password" });
     return;
   }
 
-  User.findOne({ username })
+  User.findOne({ email })
   .then(user => {
     if (user !== null) {
-      res.render("auths/signup", { message: "The username already exists" });
+      res.render("auths/signup", { message: "The email already exists" });
       return;
     }
 
@@ -39,13 +53,22 @@ router.post("/signup", (req, res, next) => {
     const hashPass = bcrypt.hashSync(password, salt);
 
     const newUser = new User({
-      username,
-      password: hashPass
+      name: name,
+      lastName: lastName,
+      password: hashPass,
+      email: email,
+      phone: phone,
+      location: location,
+      type: type,
+      personalDescription: personalDescription,
+      economicContribution: economicContribution,
+      project_id: project_id
     });
+    console.log(newUser)
+   
 
     newUser.save((err) => {
       if (err) {
-        console.log("hola")
         res.render("auths/signup", { message: "Something went wrong" });
       } else {
         res.redirect("/auths/index");
@@ -57,22 +80,25 @@ router.post("/signup", (req, res, next) => {
   })
 });
 
+
+// Log In
+
 router.get("/login", (req, res, next) => {
   res.render("auths/login");
 });
 
 
 
-// passport.serializeUser((user, cb) => {
-//   cb(null, user._id);
-// });
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
 
-// passport.deserializeUser((id, cb) => {
-//   User.findById(id, (err, user) => {
-//     if (err) { return cb(err); }
-//     cb(null, user);
-//   });
-// });
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 // passport.use(new LocalStrategy((username, password, next) => {
 //   User.findOne({ username }, (err, user) => {
@@ -90,5 +116,25 @@ router.get("/login", (req, res, next) => {
 //   });
 // }));
 
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/auths/private-page",
+  failureRedirect: "/auths/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("auths/private-page", { user: req.user });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/auths/login");
+});
 
 module.exports = router;
